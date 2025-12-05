f783891d1d8fb412c1783cb6777eee9eee47efb0
swamini10 <swaminibhandare10@gmail.com>
2025-12-05 20:47:41 +0530

    update

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatCard } from "@angular/material/card";
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MatCard],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
